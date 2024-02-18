295256298be4c86f06f8cedbd34d0f5f8d56950f
tonypdoanuchicago <tonypdoan@uchicago.edu>
2024-02-18 07:28:46 -0800

    Final Check In
    Final Check In

---
+++ Develop/PROFESSIONAL_README.md
@@ -0,0 +1,24 @@
+#How do you get a web dev job in 6 months?
+
+##Attending the Northwestern University Web Dev Bootcamp
+
+## Description
+
+Learn intensively for 6 months for a six figure job
+
+## Problem
+
+I currently want to make more money.
+
+## Solution
+
+Do the boot camp and learn the skills.
+
+## Learnings
+
+3 hours for three days per week plus challenges and project
+
+## Purpose
+
+Professional Development
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
-// TODO: Include packages needed for this application
+// TODO: DONE Include packages needed for this application
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+import * as fs from 'fs';
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt([
+    {
+        name: 'answerTitle',
+        message: 'What is the title?'
+      },
+      {
+        name: 'answerSubtitle',
+        message: 'What is the subtitle?'
+      },
+      {
+        name: 'answerDescription',
+        message: 'What is the main description?'
+      },
+      {
+        name: 'answerProblem',
+        message: 'What is the problem?'
+      },
+      {
+        name: 'answerSolution',
+        message: 'What is the solution?'
+      },
+      {
+        name: 'answerLearn',
+        message: 'What did you learn?'
+      },
+      {
+        type: 'list',
+        name: 'answerPurpose',
+        message: 'Which is the purpose of this readme?',
+        choices: ['Study', 'Professional Development', 'Work', 'Hobby'],
+      }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    var dataString = "#" + answers.answerTitle + "\n\n";
+    dataString += "##" + answers.answerSubtitle + "\n\n";
+    dataString += "## Description\n\n" + answers.answerDescription + "\n\n";
+    dataString += "## Problem\n\n" + answers.answerProblem + "\n\n";
+    dataString += "## Solution\n\n" + answers.answerSolution + "\n\n";
+    dataString += "## Learnings\n\n" + answers.answerLearn + "\n\n";
+    dataString += "## Purpose\n\n" + answers.answerPurpose + "\n\n";
+
+    writeToFile("PROFESSIONAL_README.md", dataString);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+}
 
 // Function call to initialize app
 init();
